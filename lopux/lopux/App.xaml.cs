4af63beebd5f19a6e955b526a164ede0bff8c728
Ahat <korbanov05@gmail.com>
2025-02-01 14:35:22 +0300

    start

---
+++ lopux/lopux/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using lopux.db;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace lopux
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static MainWindow frame;
+        public static LopuxDbEntities db = new LopuxDbEntities();
+    }
+}
